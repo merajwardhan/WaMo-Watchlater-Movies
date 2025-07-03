35abcda3c9844b05d2f78e199ac31659ae78d52f
imrajwardhan <patilrajwardhan7@gmail.com>
2025-07-03 08:55:32 +0000

    Added initial css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
-import './App.css'
+import './css/App.css'
 import Home from './pages/Home'
 import Favorites from './pages/favorites.jsx';
 import Saved from './pages/save.jsx'
