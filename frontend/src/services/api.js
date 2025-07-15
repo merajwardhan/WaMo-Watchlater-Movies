95cfc1cac5926ce9bea9bb6dc5bfa6bee520ada4
imrajwardhan <patilrajwardhan7@gmail.com>
2025-07-15 08:50:51 +0000

    Changed api.js to fetch the movies

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,19 +2,49 @@
 // dotenv.config();
 // need for backend not frontend
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = import.meta.env.BASE_URL;
 
 const searchTerm = ['the', 'a' , 'an' , 'of', 'in', 'and', 'man', 'day', 'night', 'love', 'dead', 'last', 'new', 'story', 'life', 'dark', 'sex'];
+const randomSearchTerm = searchTerm[Math.floor(Math.random() * searchTerm.length)];
 const pageNumber = Math.floor(Math.random() * 100) + 1;
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}?s=${searchTerm}&page=${pageNumber}`);
-  const data = await response.json();
-  return data.results;
+
+  try {
+
+    console.log(`${BASE_URL}&s=${randomSearchTerm}&page=${pageNumber}`)
+    const response = await fetch(`${BASE_URL}&s=${randomSearchTerm}&page=${pageNumber}`);
+    const data = await response.json();
+
+    if(data.Response == 'True'){
+        return data.Search;       
+    }else{
+      console.log(`OMDb Api error : ${data.error}`);
+      return [];
+    }
+
+  } catch (error) {
+    console.log(`Network Or Api error in (getPopularMovies) : ${error}`)
+    return [];
+  }
 }
 
 export const searchMovies = async (query) => {
-  const response = await fetch(`${BASE_URL}?s=${encodeURI(query)}`);
-  const data = await response.json();
-  return data.results;
+  try {
+    
+    console.log(`${BASE_URL}&s=${randomSearchTerm}&page=${pageNumber}`)
+    const response = await fetch(`${BASE_URL}&s=${encodeURI(query)}`);
+    const data = await response.json();
+
+    if(data.Response == 'True'){
+        return data.Search;       
+    }else{
+      console.log(`OMDb Api error : ${data.error}`);
+      return [];
+    }
+
+  } catch (error) {
+    console.log(`Network Or Api error in (searchMovies) : ${error}`)
+    return [];
+  }
 }
