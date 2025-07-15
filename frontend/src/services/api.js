0256b70f25cf23394860daa39fecbb416af04632
imrajwardhan <patilrajwardhan7@gmail.com>
2025-07-15 15:41:46 +0000

    Added search terms in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,10 @@
 
 const BASE_URL = import.meta.env.BASE_URL;
 
-const searchTerm = ['the', 'a' , 'an' , 'of', 'in', 'and', 'man', 'day', 'night', 'love', 'dead', 'last', 'new', 'story', 'life', 'dark', 'sex'];
+const searchTerm = ['the a' , 'of the', 'time', 'end', 'man', 'day', 'night', 'love', 'dead', 'last', 'new', 'story', 'life', 'dark', 'sex'
+  , 'age', 'dawn', 'women', 'girl', 'boy', 'love', 'heart', 'family', 'friend', 'light', 'blue', 'red', 'black', 'white', 'story', 'world'
+  , 'game', 'road', 'dream', 'truth', 'fall', 'rise', 'killer', 'war', 'battle', 'attack'
+];
 const randomSearchTerm = searchTerm[Math.floor(Math.random() * searchTerm.length)];
 const pageNumber = Math.floor(Math.random() * 100) + 1;
 
@@ -13,7 +16,7 @@ export const getPopularMovies = async () => {
   try {
 
     console.log(`${BASE_URL}&s=${randomSearchTerm}&page=${pageNumber}`)
-    const response = await fetch(`${BASE_URL}&s=${randomSearchTerm}&page=${pageNumber}`);
+    const response = await fetch(`${BASE_URL}&s=${randomSearchTerm}&page=${pageNumber}&type=movie`);
     const data = await response.json();
 
     if(data.Response == 'True'){
@@ -33,7 +36,7 @@ export const searchMovies = async (query) => {
   try {
     
     console.log(`${BASE_URL}&s=${randomSearchTerm}&page=${pageNumber}`)
-    const response = await fetch(`${BASE_URL}&s=${encodeURI(query)}`);
+    const response = await fetch(`${BASE_URL}&s=${encodeURI(query)}&type=movie`);
     const data = await response.json();
 
     if(data.Response == 'True'){
