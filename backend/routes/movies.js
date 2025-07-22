cdb2a66d17b900d4f52921d948860f5e7c58d56b
imrajwardhan <patilrajwardhan7@gmail.com>
2025-07-22 14:58:51 +0000

    feat: Created the movie fetching routes and implemented Hono route

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -1,1 +1,73 @@
 // movie related routes
+import Hono from 'hono';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// export const movieRouter = new Hono().basePath('/movie'); This results in the path being : /api/movie/movie/popular
+export const movieRouter = new Hono();
+const BASE_URL = process.env.BASE_URL_TMDB;
+const API_KEY = process.env.API_KEY_TMDB;
+
+movieRouter.get('/popular', async (c) => {
+  try {
+      const response = await fetch(`${BASE_URL}/movie/popular`, {
+        method : 'GET',
+        headers : {
+          'Content-Type' : 'application/json',
+          'Authorization' : `Bearer ${API_KEY}`
+        }
+      });
+  
+      const data = await response.json();
+  
+      if(data.results.length > 0){
+          return  c.json({
+            results : data.results
+          }, 200);       
+      }else{
+        console.log(`TMDB Api error : ${data}`);
+        return c.json({
+          results : []
+        }, 404);
+      }
+  
+    } catch (error) {
+      return c.json({
+          results : []
+      }, 400);
+    }
+})
+
+movieRouter.get('/search', async (c) => {
+  const query = c.req.query('query');
+  if(!query) return c.json({ msg : `No query found!`}, 402);
+
+  try {
+    //Page number should be a state variable or something like that so that we can go to the next page.
+    const response = await fetch(`${BASE_URL}/search/movie?query=${encodeURI(query)}&include_adult=false&language=en-US&page=1`, {
+      method : 'GET',
+      headers : {
+        'Content-Type' : 'applicaton/json',
+        'Authorization' : `Bearer ${API_KEY}`
+      }
+    });
+
+    const data = await response.json();
+
+    if(data.results.length > 0){
+        return c.json({
+          results : data.results
+        }, 200);
+    }else{
+      console.log(`TMDB Api error : ${data}`);
+      return c.json({
+        results : []
+      });
+    }
+
+  } catch (error) {
+      return c.json({
+        results : []
+      }, 200);
+    }
+})
