0f31506022c1868a6f8245aeb6833df953e4a44f
imrajwardhan <patilrajwardhan7@gmail.com>
2025-07-25 15:05:06 +0000

    feat: Added Database connection Logic

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,1 +1,25 @@
 // MongoDB connection
+
+  import mongoose from 'mongoose';
+
+  const DATABASE_NAME = "Wamo";
+  const MONGO_URL = `mongodb://localhost:27017/${DATABASE_NAME}`;
+
+  export async function connectDB() {
+    try {
+      const conn = await mongoose.connect(MONGO_URL);
+
+      console.log(`Mongo DB connected : ${conn.connection.host}`);
+
+      mongoose.connection.on('error', err => {
+        console.error(`MongoDB connection error : ${err}`);
+      })
+      mongoose.connection.on('disconnected', () => {
+        console.warn(`MongoDB disconnected!!!`);
+      })
+
+    } catch (error) {
+      console.error(`Unable to connect to MongoDB : ${error}`);
+      process.exit(1) //stop the process if the DB is unrechable
+    }
+  }
