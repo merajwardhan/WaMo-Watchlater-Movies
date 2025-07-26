930cad17d40e1e40afcb260af6c039e43ecd4344
imrajwardhan <patilrajwardhan7@gmail.com>
2025-07-26 15:37:33 +0000

    feat: Created the initial auth logic

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,1 +1,14 @@
-//JWT verification middleware
+import 'dotenv/config';
+import { sessionMiddleware } from 'hono-session';
+
+const SESSION_SECRET =  process.env.SESSION_SECRET;
+
+export const manageSession = sessionMiddleware({
+  secret : SESSION_SECRET, //This is encrypting and decrypting the cookie.
+  cookieOptions : {
+    httpOnly: true, 
+    secure : false, //true for production, allows request only from HTTPS
+    sameSite: 'lax',
+    maxAge: 24 * 60 * 60
+  }
+})
