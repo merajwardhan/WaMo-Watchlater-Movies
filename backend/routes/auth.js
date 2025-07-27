3babff39cdcbb05ad2d7ace1129eb8c141f5e928
imrajwardhan <patilrajwardhan7@gmail.com>
2025-07-27 14:12:10 +0000

    feat: Added auth routes and created authentication router

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,66 @@
-// authentication routes
+import { Hono } from 'hono';
+import { getSession } from 'hono-session';
+import 'dotenv/config';
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const authRouter = new Hono();
+
+authRouter.get('/google', (c) => {
+  const googleAuthUrl = `https://accounts.google.com/oauth/2/auth?` +
+  `client_id=${GOOGLE_CLIENT_ID}&` + 
+  `redirect_url=http://localhost:3000/api/auth/google/callback&` + 
+  `response_type=code&` +
+  `scope=email profile&` + 
+  `access_type=offline`
+
+  return c.redirect(googleAuthUrl);//when the user visits this endpoint the user is automatically redirected to the google auth website.
+})
+
+//router to handle google's callback i.e. the response that google will send
+authRouter.get('/google/callback', async (c) => {
+  const code = c.req.query('code');
+  //code is just a long random string that we recieve back from google that we can exchange for user info (only within 10 mins or so)
+  console.log(`The code object is : ${code}`);
+
+  if(!code) return c.json({ msg : `No authentication code recieved!`}, 400);
+
+  try {
+    //function that sends token for code
+    const tokenResponse = await exchangeCodeForTokens(code)
+
+    //function that returns user information
+    const userInfo = await getUserInfo(tokenResponse.access_token)
+
+    //store info in session
+    const session = await getSession(c);
+    session.set('user', {
+      id: userInfo.id,
+      email: userInfo.email,
+      name: userInfo.name,
+      picture: userInfo.name,
+      provider: 'google'
+    })
+
+    session.set('accessToken', tokenResponse.access_token)
+
+    if(tokenResponse.refresh_token) session.set('refreshToken', tokenResponse.refresh_token); //store refresh token if you want to use it.
+
+    return c.redirect('/') //return to homepage after everything is completed
+  } catch (error) {
+    console.error( `An error occured while connecting to Goolge auth : ${error}`);
+    return c.json({ 
+      msg : `Authentiction failed!`,
+      error,
+    }, 500);
+  }
+}) 
+
+authRouter.get(`/logout`, async (c) => {
+  const session = getSession(c);
+  session.delete('user');
+  session.delete('accessToken');
+  session.delete('refreshToken');
+
+  return c.redirect('/');
+})
