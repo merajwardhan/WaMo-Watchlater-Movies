3265ef0ed9021d8fc1f95f852be8442ea7e3fa7c
imrajwardhan <patilrajwardhan7@gmail.com>
2025-08-15 08:58:26 +0000

    refactor: Commented out the session management code, removing session management

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,14 +1,16 @@
-import 'dotenv/config';
-import { sessionMiddleware } from 'hono-session';
+// THIS IS THE PREVIOUS SESSION MANAGEMENT CODE WHICH IS TO BE REPLACED WITH JWT AUTH FOR SERVERLESS SUPPORT
 
-const SESSION_SECRET =  process.env.SESSION_SECRET;
-
-export const manageSession = sessionMiddleware({
-  secret : SESSION_SECRET, //This is for decrypting the session cookie
-  cookieOptions : {
-    httpOnly: true, 
-    secure : false, //true for production, allows request only from HTTPS
-    sameSite: 'lax',
-    maxAge: 24 * 60 * 60
-  }
-})
+// import 'dotenv/config';
+// import { session } from 'hono-session';
+//
+// const SESSION_SECRET =  process.env.SESSION_SECRET;
+//
+// export const manageSession = sessionMiddleware({
+//   secret : SESSION_SECRET, //This is for decrypting the session cookie
+//   cookieOptions : {
+//     httpOnly: true, 
+//     secure : false, //true for production, allows request only from HTTPS
+//     sameSite: 'lax',
+//     maxAge: 24 * 60 * 60
+//   }
+// })
