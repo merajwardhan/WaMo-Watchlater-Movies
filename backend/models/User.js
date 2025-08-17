5000a256184d4f36ad28eaec9e9acd690ecfb34b
imrajwardhan <patilrajwardhan7@gmail.com>
2025-08-17 15:52:17 +0000

    feat: added index for User schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -12,6 +12,8 @@ const UserSchema = new Schema ({
 });//basic user schema defined , make changes later . Add a ref to the movies , in such a way that you don't have to look up all the movies for a specific user.
 //Create 2 collections for saved movies and favorite movies 
 
+UserSchema.index({ googleId : 1 }); //indexing logic for finding the user.
+
 export const User = mongoose.model('Users', UserSchema);
 
 // add a clean up function in the backend that check for movies without any users and delete those movies.
