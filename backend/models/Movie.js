dbda2084cbd11566cce4030fdc57e059295ceab6
imrajwardhan <patilrajwardhan7@gmail.com>
2025-08-17 15:51:50 +0000

    feat: added index for movie schema

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -10,4 +10,6 @@ const movieSchema = new Schema({
   usersSaved : [] //if these two array are empty make a clean up function that deletes the movie from the data base.
 })
 
+movieSchema.index({ _id : 1 }); //To find the movies in the function we would need _id, we have to set ObjectId in the usersTable
+
 export const Movie = mongoose.model('Movies', movieSchema );
