832a679f4644f0e6272641c8dc9d3841bf83b935
imrajwardhan <patilrajwardhan7@gmail.com>
2025-08-17 15:11:51 +0000

    feat: Added initial Movie schema logic

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -1,1 +1,13 @@
-// Movie Schema (optional)
+import mongoose from 'mongoose';
+const { Schema } from mongoose;
+
+const movieSchema = new Schema({
+  title : String,
+  imdbID : { type : Number , unique : true },
+  poster : String ,
+  release_date : String,
+  usersFavorite : [],
+  usersSaved : [] //if these two array are empty make a clean up function that deletes the movie from the data base.
+})
+
+export const Movie = mongoose.model('Movies', movieSchema );
