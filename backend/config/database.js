cabfed5e0b7d91cc5987d0ced5e2af2a1c21a8a6
imrajwardhan <patilrajwardhan7@gmail.com>
2025-08-17 15:13:25 +0000

    minor formatting changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,25 +1,27 @@
 // MongoDB connection
 
-  import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
-  const DATABASE_NAME = "Wamo";
-  const MONGO_URL = `mongodb://172.21.16.1:27017/${DATABASE_NAME}`;
+const DATABASE_NAME = "Wamo";
+const MONGO_URL = `mongodb://172.21.16.1:27017/${DATABASE_NAME}`;
 
-  export async function connectDB() {
-    try {
-      const conn = await mongoose.connect(MONGO_URL);
+export async function connectDB() {
+  try {
+    const conn = await mongoose.connect(MONGO_URL);
 
-      console.log(`Mongo DB connected : ${conn.connection.host}`);
+    console.log(`Mongo DB connected : ${conn.connection.host}`);
 
-      mongoose.connection.on('error', err => {
-        console.error(`MongoDB connection error : ${err}`);
-      })
-      mongoose.connection.on('disconnected', () => {
-        console.warn(`MongoDB disconnected!!!`);
-      })
+    mongoose.connection.on('error', err => {
+      console.error(`MongoDB connection error : ${err}`);
+    })
+    mongoose.connection.on('disconnected', () => {
+      console.warn(`MongoDB disconnected!!!`);
+    })
 
-    } catch (error) {
-      console.error(`Unable to connect to MongoDB : ${error}`);
-      process.exit(1) //stop the process if the DB is unrechable
-    }
+  } catch (error) {
+    console.error(`Unable to connect to MongoDB : ${error}`);
+    process.exit(1) //stop the process if the DB is unrechable
   }
+}
+
+
