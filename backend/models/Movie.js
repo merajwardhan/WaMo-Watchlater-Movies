69b8ee05b2382755869c9ded2da1d28c8b8cd0e3
imrajwardhan <patilrajwardhan7@gmail.com>
2025-08-18 14:59:09 +0000

    refactor: Removed default _id indexing logic

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -10,6 +10,7 @@ const movieSchema = new Schema({
   usersSaved : [{ type : mongoose.Schema.Types.ObjectId , ref : 'Users'}] //if these two array are empty make a clean up function that deletes the movie from the data base.
 })
 
-movieSchema.index({ _id : 1 }); //To find the movies in the function we would need _id, we have to set ObjectId in the usersTable
+//movieSchema.index({ _id : 1 }); //To find the movies in the function we would need _id, we have to set ObjectId in the usersTable
+//Dont need to create indexing for "_id" as it is already indexed.
 
 export const Movie = mongoose.model('Movies', movieSchema );
