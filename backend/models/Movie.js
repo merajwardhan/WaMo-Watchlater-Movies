d8487e3a4a4904900183d2358f10674c9faa99a8
imrajwardhan <patilrajwardhan7@gmail.com>
2025-08-18 07:31:32 +0000

    fix: Added types to usersFavorits and usersSaved arrays

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -6,8 +6,8 @@ const movieSchema = new Schema({
   imdbID : { type : Number , unique : true },
   poster : String ,
   release_date : String,
-  usersFavorite : [],
-  usersSaved : [] //if these two array are empty make a clean up function that deletes the movie from the data base.
+  usersFavorite : [{ type : mongoose.Schema.Types.ObjectId , ref : 'Users'}],
+  usersSaved : [{ type : mongoose.Schema.Types.ObjectId , ref : 'Users'}] //if these two array are empty make a clean up function that deletes the movie from the data base.
 })
 
 movieSchema.index({ _id : 1 }); //To find the movies in the function we would need _id, we have to set ObjectId in the usersTable
