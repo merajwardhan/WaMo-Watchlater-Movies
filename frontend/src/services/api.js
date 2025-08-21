25aadfd0da3e526cd65c21667b2e618af0a28307
imrajwardhan <patilrajwardhan7@gmail.com>
2025-08-21 14:59:26 +0000

    fix: Removed useless VITE environment var imports

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,14 +2,11 @@
 // dotenv.config();
 // need for backend not frontend
 
-const BASE_URL = import.meta.env.VITE_BASE_URL_TMBD;
-const API_KEY = import.meta.env.VITE_API_KEY_TMDB;
-
 export const getPopularMovies = async () => {
   try {
     const response = await fetch('http://localhost:3000/api/movie/popular');
     const data = await response.json();
-
+    
     if(data.details.results.length > 0) return data.details;
     else return [];
   } catch (error) {
