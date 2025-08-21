9b613826f784cf6d11ffb77599d5cbca5342e016
imrajwardhan <patilrajwardhan7@gmail.com>
2025-08-21 14:56:46 +0000

    feat: Added User logic component

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/NavBar.css';
 
 export default function NavBar(){
+
+  const [ user , setUser ] = useState(null);
+
     return <>
     <nav className="navBar">
       <div className='navBarBrand'>
-        <Link to='/'>Movie App</Link>
+        <Link to='/'>WaMo - Watch Movies Later</Link>
       </div>
       <div className='navBarLinks'>
         <Link to='/' className='navLink'>Home</Link>
         <Link to='/favorites' className='navLink'>Favorites</Link>
         <Link to='/saved' className='navLink'>Saved</Link>
+        { user ? (
+          <span className='navLinkUser' >Welcome , {user.name}</span>
+        ) : (
+          <a href="/auth/google" className='navLinkUser'>Login with google</a>
+        )}
       </div>
     </nav>
     </>
