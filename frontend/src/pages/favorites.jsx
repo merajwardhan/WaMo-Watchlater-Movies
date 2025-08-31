4d2a91a6232c9caa76adc5de4a5bb4e28de2cc9e
imrajwardhan <patilrajwardhan7@gmail.com>
2025-08-31 15:33:07 +0000

    feat: Created stated and intial structure for favorite movies fetching

--- frontend/src/pages/favorites.jsx
+++ frontend/src/pages/favorites.jsx
@@ -1,10 +1,37 @@
 import '../css/Favorites.css'
+import MovieCard from '../components/MovieCard.jsx';
+import { searchFavoriteMovies } from '../services/api.js';
+import { useState , useEffect } from 'react';
 
 export default function Favorites(){
-    return <>
-    <div className = 'favorites'>
-      <h2>No Favorite movies yet!</h2>
-      <p>When you favorite the movies they will be added here!</p>
-    </div>
-    </>
+
+  const [ movies , setMovies ] = useState([]);
+  const [ error , setError ] = useState("");
+  const [ page , setPage ] = useState(1);
+  const [ loading , setLoading ] = useState(true); 
+  const [ totalpages , setTotalPages ] = useState(0);
+
+  useEffect( () => {
+    try {
+      
+      const favMovies = await searchFavoriteMovies();
+      
+    } catch (error) {
+      console.log(`Error while fetching the favorite movies , Error = ${error}`)
+      setError(`Failed to load your favorite movies : ( \nPlease Try again!`);
+      setMovies([])
+    } finally{
+      setLoading(false);
+    }
+  }, [] )
+
+  return <>
+    
+  </>
 }
+
+
+    {/* <div className = 'favorites'> */}
+    {/*   <h2>No Favorite movies yet!</h2> */}
+    {/*   <p>When you favorite the movies they will be added here!</p> */}
+    {/* </div> */}
