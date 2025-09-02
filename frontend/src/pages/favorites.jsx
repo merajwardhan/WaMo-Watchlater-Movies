b8744985a9b9adced7a3210548c690c936aad3bd
imrajwardhan <patilrajwardhan7@gmail.com>
2025-09-02 14:05:03 +0000

    Added fetching favorites logic in useEffect

--- frontend/src/pages/favorites.jsx
+++ frontend/src/pages/favorites.jsx
@@ -12,26 +12,48 @@ export default function Favorites(){
   const [ totalpages , setTotalPages ] = useState(0);
 
   useEffect( () => {
-    try {
-      
-      const favMovies = await searchFavoriteMovies();
+    const fetchFavorites = async () => {
+      try {
 
-      if(favMovies.length > 0) setMovies(favMovies);
-      else setError(`Unable to find your favorite movies,\nPlease set your favorite movies!`);
-      
-    } catch (error) {
-      console.log(`Error while fetching the favorite movies , Error = ${error}`)
-      setError(`Failed to load your favorite movies : ( \nPlease Try again!`);
-      setMovies([])
-    } finally{
-      setLoading(false);
+        const favMovies = await searchFavoriteMovies();
+
+        if(favMovies.length > 0) setMovies(favMovies);
+        else setError(`Unable to find your favorite movies,\nPlease set your favorite movies!`);
+
+      } catch (error) {
+        console.log(`Error while fetching the favorite movies , Error = ${error}`)
+        setError(`Failed to load your favorite movies : ( \nPlease Try again!`);
+        setMovies([])
+      } finally{
+        setLoading(false);
+      }
     }
+
+    fetchFavorites();
   }, [] )
+  
+  if(loading){
+    return <>
+      <div className = 'favorites'>
+        <h2>Loading content please wait with your popcorns!</h2>
+        <p>Take a seat, it's entertainment time.</p>
+      </div>
+    </>
+  }
+
+  if(error){
+    return <>
+      <div className = 'favorites'>
+        <h2>Something went wrong while loading the contents!</h2>
+        <p>ERROR : {error}</p>
+      </div>
+    </>
+  }
 
   return <>
-    { movies.length > 1 ? 
+    { movies.length > 0 ? 
     <div className='moviesGrid'>
-      {movies.map((mov) => (<SavedMovieCard {..mov} key={mov.imdbID}/>))}
+      {movies.map((mov) => (<SavedMovieCard {...mov} key={mov.imdbID}/>))}
     </div>} : 
     <div className="favorites">
       <h2>No Favorite movies yet!</h2>
