2cc77cf299c1976a8311cfaaad3eca3900e44721
imrajwardhan <patilrajwardhan7@gmail.com>
2025-09-30 11:32:10 +0000

    feat: added the logoutUser function for navbar component

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,10 +2,26 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/NavBar.css';
 import { useUser } from '../contexts/UserContext.jsx';
-
+import { logUserOut } from '../services/api.js';
+import { toast } from 'react-toastify';
 export default function NavBar(){
 
-  const { user, loading } = useUser(); //Destructure the created custom hook
+  const { user, setUser, loading } = useUser(); //Destructure the created custom hook
+
+  async function logoutUser(){
+    try {
+      const loggedOut = await logUserOut(); 
+
+      if(loggedOut){
+        toast(`User has been logged out!`)
+        setUser(null);
+      }
+      else toast(`Failed to log you out, try again later or contact the developer!`)
+
+    } catch (error) {
+      toast(`Error has occured \nFailed to log you out, try again later or contact the developer!`)
+    }
+  }
 
     return <>
     <nav className="navBar">
@@ -20,7 +36,10 @@ export default function NavBar(){
           <span className='navLinkUser' >Loading!!!</span>
         ) : ( user ? 
             (
-              <span className='navLinkUser' >Welcome , {user.name}</span>
+              <>
+                <span className='navLinkUser'>Welcome , {user.name}</span>
+                <button onClick={logoutUser} className='searchButton'>Log out!</button>
+              </>
             ) : (
               <a href="http://localhost:3000/api/auth/google" className='navLinkUser'>Login with google</a>
             )
